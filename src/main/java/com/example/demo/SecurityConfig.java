144b842b6ad0298ef26af222831b62f04c866204
webdoxinfotech <test.webdox@gmail.com>
2024-03-19 23:52:12 +0530

    jdbc users

--- src/main/java/com/example/demo/SecurityConfig.java
+++ src/main/java/com/example/demo/SecurityConfig.java
@@ -1,15 +1,27 @@
 package com.example.demo;
 
+import javax.sql.DataSource;
+
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer.FrameOptionsConfig;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.jdbc.JdbcDaoImpl;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
 
 @Configuration
 @EnableWebSecurity
@@ -20,28 +32,64 @@ public class SecurityConfig {
         http
                 .authorizeHttpRequests((authorize) -> authorize
                         .requestMatchers("/").permitAll()
+                        .requestMatchers(PathRequest
+                                .toH2Console()).permitAll()
                         .requestMatchers("admin/**").hasAnyRole("USER")
                         .anyRequest().authenticated())
+
+
                 .httpBasic(Customizer.withDefaults())
                 // .formLogin(Customizer.withDefaults());
                 .formLogin(form -> form
-                                .loginPage("/login")
-                                .permitAll()
-                ).csrf(csrf -> csrf.disable()).logout((logout) -> logout.logoutSuccessUrl("/logged-out").permitAll());
-                
+                        .loginPage("/login")
+                        .permitAll())
+                .csrf(csrf -> csrf.disable()).logout((logout) -> logout.logoutSuccessUrl("/logged-out").permitAll())
+                .csrf(csrf -> csrf
+            .ignoringRequestMatchers(PathRequest.toH2Console())
+            .disable())
+            .headers(headers -> headers.frameOptions(FrameOptionsConfig::disable));
+
 
         return http.build();
     }
 
+    // @Bean
+    // public UserDetailsService userDetailsService() {
+    //     UserDetails userDetails = User.withDefaultPasswordEncoder()
+    //             .username("user")
+    //             .password("password")
+    //             .roles("USER")
+    //             .build();
+
+    //     return new InMemoryUserDetailsManager(userDetails);
+    // }
+
     @Bean
-    public UserDetailsService userDetailsService() {
-        UserDetails userDetails = User.withDefaultPasswordEncoder()
+    DataSource dataSource() {
+        return new EmbeddedDatabaseBuilder()
+                .setType(EmbeddedDatabaseType.H2)
+                .addScript(JdbcDaoImpl.DEFAULT_USER_SCHEMA_DDL_LOCATION)
+                .build();
+    }
+
+    @Bean
+    UserDetailsManager users(DataSource dataSource) {
+        UserDetails user = User.builder()
                 .username("user")
-                .password("password")
+                .password("{bcrypt}$2a$10$GRLdNijSQMUvl/au9ofL.eDwmoohzzS7.rmNSJZ.0FxO/BTk76klW")
                 .roles("USER")
                 .build();
-
-        return new InMemoryUserDetailsManager(userDetails);
+        UserDetails admin = User.builder()
+                .username("admin")
+                .password("{bcrypt}$2a$10$GRLdNijSQMUvl/au9ofL.eDwmoohzzS7.rmNSJZ.0FxO/BTk76klW")
+                .roles("USER", "ADMIN")
+                .build();
+        JdbcUserDetailsManager users = new JdbcUserDetailsManager(dataSource);
+        users.createUser(user);
+        users.createUser(admin);
+        return users;
     }
+    
+
 
 }
